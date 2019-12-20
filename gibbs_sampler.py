c63b31fa9c1e33d7d82d5f4993c1f13daaab34f0
anazaret <achille.nazaret@gmail.com>
2019-12-19 23:01:39 -0500

    THE Gibbs

--- gibbs_sampler.py
+++ gibbs_sampler.py
@@ -1,74 +1,69 @@
 import numpy as np
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.model_selection import KFold
+from sklearn.neighbors import NearestNeighbors
 from scipy.stats import dirichlet, norm
 import numpy_indexed as npi
+from scipy.stats import mode
+from sklearn.metrics import normalized_mutual_info_score
 
 class GibbsSampler:
-    def __init__(
-        self,
-        num_cells,
-        num_cell_types,
-        num_genes,
-        sigma,
-        S,
-        epsilon,
-        graph,
-        graph_reverse,
-        alpha=None,
-    ):
-        self.num_cells = num_cells
-        self.num_cell_types = num_cell_types
-        self.num_genes = num_genes
-        self.epsilon = epsilon
-        self.graph = graph
-        self.graph_reverse = graph_reverse
+    def __init__(self, data_generator, sigma=10, S=1, alpha=None):
+        self.n_cells = dg.n_cells
+        self.n_cell_types = dg.n_cell_types
+        self.n_genes = dg.n_genes
+        self.epsilon = dg.epsilon
+        self.graph = dg.graph
+        self.graph_reversed = dg.graph_reversed
+        self.dg = data_generator
+
         self.epsilon_up = self.epsilon / self.graph.shape[1]
         self.sigma = sigma
         self.S = S
         self.Vi = None
         self.neighbors_cell_types = None
         if alpha is None:
-            alpha = np.ones(num_cell_types)
+            alpha = np.ones(dg.n_cell_types)
         self.alpha = alpha
+        self.cluster_truth = dg.cell_types
         self.reset()
 
     def reset(self):
         self.pi = np.random.dirichlet(self.alpha)
-        self.update_Z(
-            np.random.choice(self.num_cell_types, p=self.pi, size=self.num_cells)
-        )
+        self.Z = np.random.choice(self.n_cell_types, p=self.pi, size=self.n_cells)
         self.beta = np.random.normal(
-            0, self.sigma, size=(self.num_cell_types, self.num_genes)
+            0, self.sigma, size=(self.n_cell_types, self.n_genes)
         )
         self.rho = np.random.normal(
-            0,
-            self.sigma,
-            size=(self.num_cell_types, self.num_cell_types, self.num_genes),
+            0, self.sigma, size=(self.n_cell_types, self.n_cell_types, self.n_genes)
         )
+        self.update_intermediaries()
         self.log_joints = []
+        self.cluster_score = []
 
-    def update_Z(self, Z):
-        self.Z = Z.astype(int)
-
+    def update_intermediaries(self):
         # Count types
-        self.n_i = np.zeros(self.num_cell_types)
+        self.n_i = np.zeros(self.n_cell_types)
         cell_types, counts = np.unique(self.Z, return_counts=True)
         self.n_i[cell_types] = counts
 
         # Reverse map each type l: list of cells having type l
-        values, indices = npi.group_by(self.Z, np.arange(self.num_cells))
-        self.Vi = [np.array([]) for _ in range(self.num_cell_types)]
+        values, indices = npi.group_by(self.Z, np.arange(self.n_cells))
+        self.Vi = [np.array([]) for _ in range(self.n_cell_types)]
         for i, v in enumerate(values):
             self.Vi[v] = indices[i]
 
         # For each cell v, number of neighbors of type k
-        self.mvk = np.zeros((self.num_cells, self.num_cell_types))
-        for i in range(self.num_cells):
+        self.mvk = np.zeros((self.n_cells, self.n_cell_types))
+        for i in range(self.n_cells):
             cell_types, counts = np.unique(self.Z[self.graph[i]], return_counts=True)
             self.mvk[i][cell_types] = counts
 
         # Count number of edge k->l (in l,k)
-        self.ctype_ctype_counts = np.zeros((self.num_cell_types, self.num_cell_types))
-        for l in range(self.num_cell_types):
+        self.ctype_ctype_counts = np.zeros((self.n_cell_types, self.n_cell_types))
+        for l in range(self.n_cell_types):
             if len(self.Vi[l]):
                 self.ctype_ctype_counts[l] = self.mvk[self.Vi[l]].sum(axis=0)
 
@@ -84,7 +79,7 @@ class GibbsSampler:
                 (x[self.Vi[i]].sum(axis=0) if len(self.Vi[i]) else 0)
                 - self.epsilon_up
                 * (self.rho[i, :, :] * self.ctype_ctype_counts[i, :, None]).sum(axis=0)
-                for i in range(self.num_cell_types)
+                for i in range(self.n_cell_types)
             ]
         )
 
@@ -92,9 +87,7 @@ class GibbsSampler:
             multipliers[:, None] * alphas, np.sqrt(variances)[:, None]
         )
 
-    def sample_rho(self, x):
-        self.mvk
-
+    def update_rho(self, x):
         eta = 1.0 / (
             self.S ** 2 / self.sigma ** 2
             + self.epsilon_up ** 2
@@ -102,14 +95,15 @@ class GibbsSampler:
                 [
                     (self.mvk[self.Vi[i]] ** 2).sum(axis=0)
                     if len(self.Vi[i])
-                    else np.zeros((self.num_cell_types))
-                    for i in range(self.num_cell_types)
+                    else np.zeros((self.n_cell_types))
+                    for i in range(self.n_cell_types)
                 ]
             )
         )
 
-        t = self.epsilon_up * np.array(
-            [
+        for k in range(self.n_cell_types):
+
+            t = self.epsilon_up * np.array(
                 [
                     np.sum(
                         (
@@ -128,19 +122,19 @@ class GibbsSampler:
                         * (self.Z == i)[:, None],
                         axis=0,
                     )
-                    for k in range(self.num_cell_types)
+                    for i in range(self.n_cell_types)
                 ]
-                for i in range(self.num_cell_types)
-            ]
-        )
+            )
+            # t: ij
+            self.rho[:, k, :] = np.random.normal(
+                eta[:, k, None] * t, np.sqrt((self.S ** 2) * eta[:, k, None])
+            )
 
-        #         for i in range(self.num_cell_types):
+        #         for i in range(self.n_cell_types):
         #             mu[i] = self.epsilon_up * (
-        #                 self.mvk[self.Vi[i]].sum(axis=0) if len(self.Vi[i]) else np.zeros((self.num_cell_types))
+        #                 self.mvk[self.Vi[i]].sum(axis=0) if len(self.Vi[i]) else np.zeros((self.n_cell_types))
         #             )
-        return np.random.normal(
-            eta[:, :, None] * t, np.sqrt((self.S ** 2) * eta[:, :, None])
-        )
+        return self.rho
 
     def log_pz(self, x, v, k):
         # logp(z_v = k) + constant
@@ -149,12 +143,12 @@ class GibbsSampler:
                 self.beta[k]
                 - x[v]
                 + self.epsilon_up
-                * self.rho[self.Z[v]][self.Z[self.graph[v]]].sum(axis=0)
+                * self.rho[k][self.Z[self.graph[v]]].sum(axis=0)
             )
             ** 2
         ).sum()
 
-        neighbors = self.graph_reverse[v]
+        neighbors = self.graph_reversed[v]
         if len(neighbors):
             zw = self.Z[neighbors]
             old_zv = self.Z[v]
@@ -178,40 +172,36 @@ class GibbsSampler:
 
         return term1 + term2
 
-    def sample_Z(self, x):
-        log_vals = np.array(
-            [
+    def update_Z(self, x):
+        for i in range(self.n_cells):
+            log_vals = np.array(
                 [
-#                                         self._log_joint(
-#                                             x, self.Z + (k - self.Z[i]) * (np.arange(self.num_cells) == i)
-#                                         )
-                    - self.log_pz(x, i, k) + np.log(self.pi[k])
-                    for k in range(self.num_cell_types)
+                    -self.log_pz(x, i, k) + np.log(self.pi[k])
+                    for k in range(self.n_cell_types)
                 ]
-                for i in range(self.num_cells)
-            ]
-        )
-        log_vals -= np.max(log_vals, axis=1)[:, None]
-        probas = np.exp(log_vals) / np.sum(np.exp(log_vals), axis=1)[:, None]
-        return np.array(
-            [
-                np.random.choice(np.arange(self.num_cell_types), p=probas[v])
-                for v in range(self.num_cells)
-            ]
-        )
-
+            )
+            log_vals -= np.max(log_vals)
+            probas = np.exp(log_vals) / np.sum(np.exp(log_vals))
+            
+            self.Z[i] = np.random.choice(np.arange(self.n_cell_types), p=probas)
+        
     """
         This function computes the log-joint for a specified Z, which is used while sampling Z. 
         To get the log-joint for the current value of the parameters, use log_joint
     """
 
     def _log_joint(self, x, Z):
-        temp = np.sum(np.log(self.pi[Z]))
+        temp = (
+            dirichlet.logpdf(self.pi, self.alpha)
+            + np.sum(np.log(self.pi[Z]))
+            + np.sum(norm.logpdf(self.beta, 0, self.sigma))
+            + np.sum(norm.logpdf(self.rho, 0, self.sigma))
+        )
         loc = np.array(
             [
                 self.beta[Z[v], :]
                 + self.epsilon_up * np.sum(self.rho[Z[v], Z[self.graph[v]], :], axis=0)
-                for v in range(self.num_cells)
+                for v in range(self.n_cells)
             ]
         )
         return temp + np.sum(norm.logpdf(x, loc, self.S * np.ones(loc.shape)))
@@ -219,21 +209,33 @@ class GibbsSampler:
     def log_joint(self, x):
         return self._log_joint(x, self.Z)
 
-    def train_(self, num_epochs, x):
-        for _ in range(num_epochs):
-            self.update_Z(self.sample_Z(x))
-            self.pi = self.sample_pi()
-            self.beta = self.sample_beta(x)
-            self.rho = self.sample_rho(x)
-            self.log_joints.append(self.log_joint(x))
+    def cold_start(self, x, n_epochs=10, n_samples=15, seed=0):
+        scores = []
+        for i in range(n_samples):
+            np.random.seed(seed + i)
+            self.reset()
+            self.train(n_epochs, x, False)
+            scores.append((self.log_joint(x), i))
+        _, i = max(scores)
 
-    def train(self, num_epochs, x):
-        for _ in range(num_epochs):
-            self.update_Z(self.sample_Z(x))
+        np.random.seed(seed + i)
+        self.reset()
+        self.train(n_epochs, x)
+
+        return max(scores), min(scores)
+
+    def train(self, num_epochs, x, log_scores=True):
+        for ep in range(num_epochs):
+            self.update_Z(x)
+            self.update_intermediaries()
             self.pi = self.sample_pi()
             self.beta = self.sample_beta(x)
-            self.rho = self.sample_rho(x)
-            self.log_joints.append(self.log_joint(x))
+            self.update_rho(x)
+            if log_scores and (ep + 1) % 5 == 0:
+                self.log_joints.append(self.log_joint(x))
+                self.cluster_score.append(
+                    normalized_mutual_info_score(self.Z, self.cluster_truth)
+                )
 
     def plot(self):
         plt.plot(np.arange(len(self.log_joints)), self.log_joints)
